71f0e6f9f9f91d6ccf32fe1479d20712e91687f6
Tamal Saha <tamal@appscode.com>
2021-09-21 08:47:00 -0700

    Log warning if Community License is used with non-demo namespace
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -6,7 +6,7 @@ SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
 PR_BRANCH=kubevault-repo-refresher # -$(date +%s)
-COMMIT_MSG="Update dependencies"
+COMMIT_MSG="Log warning if Community License is used with non-demo namespace"
 
 REPO_ROOT=/tmp/kubevault-repo-refresher
 
@@ -37,7 +37,7 @@ refresh() {
             go mod tidy
         fi
         go mod edit \
-            -require=kmodules.xyz/client-go@15cac6c74063a9893fe295f4f8bd749b00558ca2 \
+            -require=kmodules.xyz/client-go@f005c6dfcb323e650d7b4ef61b2588686c2b9f57 \
             -require=kmodules.xyz/monitoring-agent-api@0323c6034f702a930f63a38e52dd7535b3534622 \
             -require=kmodules.xyz/webhook-runtime@f6ab57afa7e47c6379d41bfefda37e260e7274c3 \
             -require=kmodules.xyz/resource-metadata@8f6a3d706da589c610fd36f90a2fd1277bfc45c5 \
