87f6664a54ec0d59831fbbe1264591619d5f5615
Tamal Saha <tamal@appscode.com>
2022-02-07 13:12:12 -0800

    Publish GenericResource
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -6,11 +6,11 @@ SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
 PR_BRANCH=kubevault-repo-refresher # -$(date +%s)
-COMMIT_MSG="Update dependencies"
+COMMIT_MSG="Publish GenericResource"
 
 REPO_ROOT=/tmp/kubevault-repo-refresher
 
-KUBEDB_API_REF=${KUBEDB_API_REF:-master}
+KUBEVAULT_API_REF=${KUBEVAULT_API_REF:-master}
 
 repo_uptodate() {
     # gomodfiles=(go.mod go.sum vendor/modules.txt)
@@ -33,26 +33,28 @@ refresh() {
     if [ -f go.mod ]; then
         if [ "$1" != "github.com/kubevault/apimachinery" ]; then
             go mod edit \
-                -require kubevault.dev/apimachinery@${KUBEDB_API_REF}
+                -require kubevault.dev/apimachinery@${KUBEVAULT_API_REF}
             go mod tidy
         fi
         go mod edit \
-            -require=kmodules.xyz/client-go@0cf6ea46b0306b255a82f14b00b374027b0e9192 \
-            -require=kmodules.xyz/monitoring-agent-api@38ca075a2dbde85cf48d84b699720925066a5f3a \
-            -require=kmodules.xyz/webhook-runtime@7f73c2ab318a43feb61f11696815d2abdc745af1 \
-            -require=kmodules.xyz/resource-metadata@v0.6.4 \
-            -require=kmodules.xyz/custom-resources@72bd9e8cae6e8ca708e6e716bef12a2f58f60b96 \
-            -require=kmodules.xyz/objectstore-api@fdf68f88ea6e6b92a3c31339128b3551e2bc9742 \
-            -require=go.bytebuilders.dev/license-verifier@v0.9.3 \
-            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.9.3 \
-            -require=go.bytebuilders.dev/audit@v0.0.11 \
-            -require=gomodules.xyz/x@v0.0.8 \
+            -require=kmodules.xyz/client-go@1de48437aaf3867c0cafe186b3d2d5fad6ffeabe \
+            -require=kmodules.xyz/monitoring-agent-api@028e6430395e6ad760aaa337b2e678939f6de3d3 \
+            -require=kmodules.xyz/webhook-runtime@909a755cc9d1068720bad9907a9b0c488a2d0f92 \
+            -require=kmodules.xyz/resource-metadata@v0.9.3 \
+            -require=kmodules.xyz/custom-resources@be014631db0c78fd594f74647f7ea78d94a01b55 \
+            -require=kmodules.xyz/objectstore-api@3271069de43e767f91aca914d44ad335a9abc58c \
+            -require=kmodules.xyz/offshoot-api@cc7187e020cfd9931b5e97efcd04ec4de7a998da \
+            -require=go.bytebuilders.dev/license-verifier@v0.9.7 \
+            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.9.7 \
+            -require=go.bytebuilders.dev/audit@v0.0.18 \
+            -require=gomodules.xyz/x@v0.0.10 \
+            -require=gomodules.xyz/logs@v0.0.6 \
             -replace=github.com/satori/go.uuid=github.com/gomodules/uuid@v4.0.0+incompatible \
             -replace=github.com/dgrijalva/jwt-go=github.com/gomodules/jwt@v3.2.2+incompatible \
             -replace=github.com/golang-jwt/jwt=github.com/golang-jwt/jwt@v3.2.2+incompatible \
             -replace=github.com/form3tech-oss/jwt-go=github.com/form3tech-oss/jwt-go@v3.2.5+incompatible \
             -replace=helm.sh/helm/v3=github.com/kubepack/helm/v3@v3.6.1-0.20210518225915-c3e0ce48dd1b \
-            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.21.2-0.20210716212718-83e5493ac170
+            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.21.2-0.20220112070009-e3f6e88991d9
         go mod tidy
         go mod vendor
     fi
