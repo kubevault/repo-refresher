acefc934b52757750cad11f439646f42d991c6ab
Tamal Saha <tamal@appscode.com>
2023-09-07 04:59:38 -0700

    Update deps
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- dep-refresh.sh
+++ dep-refresh.sh
@@ -6,7 +6,7 @@ SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
 PR_BRANCH=kdbdeps # -$(date +%s)
-COMMIT_MSG="Update license verifier"
+COMMIT_MSG="Update deps"
 
 REPO_ROOT=/tmp/licup22
 
@@ -34,15 +34,25 @@ refresh() {
 
     if [ -f go.mod ]; then
         go mod edit \
-            -require=github.com/docker/distribution@v2.8.2+incompatible \
-            -require=kmodules.xyz/client-go@v0.25.23 \
+            -require=gomodules.xyz/logs@v0.0.7 \
+            -require=kubedb.dev/apimachinery@8e2aab0c176e12bc3c0ff0ee4b732bd7faddd0ed \
+            -require=kubedb.dev/db-client-go@6ddd035705ef3af7d835fb93611b92f9a1e729f5 \
+            -require=k8s.io/kube-openapi@v0.0.0-20220803162953-67bda5d908f1 \
+            -require=kmodules.xyz/client-go@v0.25.32 \
+            -require=kmodules.xyz/resource-metadata@v0.17.12 \
+            -require=kmodules.xyz/go-containerregistry@v0.0.11 \
+            -replace=github.com/Masterminds/sprig/v3=github.com/gomodules/sprig/v3@v3.2.3-0.20220405051441-0a8a99bac1b8 \
             -require=gomodules.xyz/password-generator@v0.2.9 \
             -require=go.bytebuilders.dev/license-verifier@v0.13.2 \
             -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.13.2 \
             -require=go.bytebuilders.dev/audit@v0.0.27 \
-            -require=stash.appscode.dev/apimachinery@v0.30.0 \
-            -require=github.com/redis/go-redis/v9@v9.0.5 \
-            -require=github.com/elastic/go-elasticsearch/v7@v7.15.1
+            -require=stash.appscode.dev/apimachinery@v0.31.0 \
+            -require=github.com/elastic/go-elasticsearch/v7@v7.15.1 \
+            -require=go.mongodb.org/mongo-driver@v1.10.2 \
+            -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@ac-0.13.0 \
+            -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.6 \
+            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@ac-1.25.1 \
+            -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@ac-1.25.1
 
         # sed -i 's|NewLicenseEnforcer|MustLicenseEnforcer|g' `grep 'NewLicenseEnforcer' -rl *`
         go mod tidy
